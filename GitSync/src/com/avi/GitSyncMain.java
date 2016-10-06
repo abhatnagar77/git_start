09ab91d33ac05267ba6f5e13a12a91aa177952cd
Avi Bhatnagar <avneesh_bhatnagar@hotmail.com>
2016-10-06 16:37:48 -0500

    Initial

---
+++ GitSync/src/com/avi/GitSyncMain.java
@@ -0,0 +1,14 @@
+package com.avi;
+
+public class GitSyncMain {
+
+	public GitSyncMain() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Gitsync active");
+	}
+
+}
